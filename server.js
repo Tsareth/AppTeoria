dea4212d5d1ab4ec2dd6ef27474f9b0b0b8ccaa7
Tsareth <55133054+Tsareth@users.noreply.github.com>
2021-01-16 20:15:18 -0600

    ProyectoTeoria 1.04

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 const express = require('express');
 const cors = require('cors')
-
+const logger = require('morgan');
+const bodyParser = require('body-parser');
 var app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+//quitar en producción
+app.use(logger('dev'));
+
 app.use(cors());
 const PORT = process.env.PORT || 8008
 
@@ -13,14 +20,15 @@ app.listen(PORT,function(){
 })
 
 app.get('/', function(req, res) {
-    res.sendFile('index.html', { root: __dirname });
+    res.sendFile('index.html    ', { root: __dirname });
 });
 
-app.post('/getDisease', function(req, res){
-    console.log(req.body);
+app.put('/get-disease', function(req, res){
+    console.log(req)
     const { imageFile } = req.body;
     try{
         console.log(imageFile);
+        return res.json({success: true})
     }catch(err){
         return res.json({success: false, error: err})
     }

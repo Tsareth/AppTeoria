d1d672a37fe8baee9e7530569d8d75470e2d0099
Tsareth <55133054+Tsareth@users.noreply.github.com>
2021-01-16 19:45:59 -0600

    Update Server

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const cors = require('cors')
 
 var app = express();
 app.use(cors());
-const PORT = process.env.PORT || 8008
+const PORT = 3000;
 app.use(express.static(__dirname));
 
 app.listen(PORT,function(){
@@ -11,6 +11,16 @@ app.listen(PORT,function(){
     console.log("URL: http://localhost:"+PORT)
 })
 
-app.all('*', function(req, res) {
+app.get('/', function(req, res) {
     res.sendFile('index.html', { root: __dirname });
 });
+
+app.post('/getDisease', function(req, res){
+    console.log(req.body);
+    const { imageFile } = req.body;
+    try{
+        console.log(imageFile);
+    }catch(err){
+        return res.json({success: false, error: err})
+    }
+})
